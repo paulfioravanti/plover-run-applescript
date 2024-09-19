c1bb5fbb531e803f57045f1e2681428b9577cb20
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-09-19 22:33:00 +1000

    Extract config functionality out into extractors and loaders

--- plover_run_applescript/config/__init__.py
+++ plover_run_applescript/config/__init__.py
@@ -5,7 +5,7 @@ A package dealing with:
     - loading and saving config containing filepath pointers to compiled
       AppleScript files
 """
-from .actions import (
+from .loader import (
     load,
     save
 )
--- plover_run_applescript/path/expander.py
+++ plover_run_applescript/path/expander.py
@@ -3,7 +3,6 @@ Expander - a module for dealing with expansion of ENV vars in a file path.
 """
 import os
 import re
-
 from typing import (
     Pattern,
     Tuple
@@ -11,7 +10,9 @@ from typing import (
 
 
 _ENV_VAR: Pattern[str] = re.compile(r"(\$[A-Za-z_][A-Za-z_0-9]*)")
-_DEFAULT_SHELL: str = "bash"
+# NOTE: Entire shell path cannot be used because Plover's shell location may
+# not be the same as the user's machine.
+_SHELL: str = os.getenv("SHELL", "bash").split("/")[-1]
 _VAR_DIVIDER: str = "##"
 _ENV_VAR_SYNTAX: str = "$"
 
@@ -22,12 +23,11 @@ def expand(path: str) -> str:
     Raises an error if a value for the env var cannot be found.
     """
     parts: list[str] = re.split(_ENV_VAR, path)
-    shell: str = _fetch_shell()
     expanded_parts: list[str] = []
 
     for part in parts:
         if part.startswith(_ENV_VAR_SYNTAX):
-            expanded_parts.append(_perform_expansion(part, shell))
+            expanded_parts.append(_perform_expansion(part))
         else:
             expanded_parts.append(part)
 
@@ -39,26 +39,19 @@ def expand_list(filepath_list: list[str]) -> list[Tuple[str, str]]:
 
     Removes a filepath from the list if its value is blank.
     """
-    filepaths = _VAR_DIVIDER.join(filepath_list)
-    shell = _fetch_shell()
-    expanded_filepaths = _perform_expansion(filepaths, shell)
-    expanded_filepath_list = list(zip(
-        filepath_list,
-        expanded_filepaths.split(_VAR_DIVIDER)
-    ))
+    filepaths: str = _VAR_DIVIDER.join(filepath_list)
+    expanded_filepaths: str = _perform_expansion(filepaths)
+    expanded_filepath_list: list[Tuple[str, str]] = list(
+        zip(filepath_list, expanded_filepaths.split(_VAR_DIVIDER))
+    )
 
     return expanded_filepath_list
 
-def _fetch_shell() -> str:
-    # NOTE: Entire shell path cannot be used because Plover's shell location may
-    # not be the same as the user's machine.
-    return os.getenv("SHELL", _DEFAULT_SHELL).split("/")[-1]
-
-def _perform_expansion(target: str, shell: str) -> str:
+def _perform_expansion(target: str) -> str:
     # NOTE: Using an interactive mode command (bash/zsh/fish -ic) seemed to be
     # the only way to access a user's env vars on a Mac outside Plover's
     # environment.
-    expanded: str = os.popen(f"{shell} -ic 'echo {target}'").read().strip()
+    expanded: str = os.popen(f"{_SHELL} -ic 'echo {target}'").read().strip()
 
     if not expanded:
         raise ValueError(f"No value found for env var: {target}")
