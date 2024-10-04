99d90e73ef0b4165c63c3f0dd3673ee0528bb439
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-10-04 11:46:28 +1000

    Refactor path fixtures

--- test/config/conftest.py
+++ test/config/conftest.py
@@ -5,36 +5,27 @@ import pytest
 
 @pytest.fixture
 def bad_config_path():
-    return (Path(__file__).parent / "files/bad_json_data.json").resolve()
+    return _path("files/bad_json_data.json")
 
 @pytest.fixture
 def non_existent_config_path():
-    return (Path(__file__).parent / "files/non_existent.json").resolve()
+    return _path("files/non_existent.json")
 
 @pytest.fixture
 def non_list_applescript_filepaths_config_path():
-    return (
-        Path(__file__).parent / "files/non_list_applescript_filepaths.json"
-    ).resolve()
+    return _path("files/non_list_applescript_filepaths.json")
 
 @pytest.fixture
 def empty_list_applescript_filepaths_config_path():
-    return (
-        Path(__file__).parent / "files/empty_list_applescript_filepaths.json"
-    ).resolve()
+    return _path("files/empty_list_applescript_filepaths.json")
 
 @pytest.fixture
 def list_non_string_applescript_filepaths_config_path():
-    return (
-        Path(__file__).parent
-        / "files/list_non_string_applescript_filepaths.json"
-    ).resolve()
+    return _path("files/list_non_string_applescript_filepaths.json")
 
 @pytest.fixture
 def valid_applescript_filepaths_config_path():
-    path = (
-        Path(__file__).parent / "files/valid_applescript_filepaths.json"
-    ).resolve()
+    path = _path("files/valid_applescript_filepaths.json")
 
     with path.open(encoding="utf-8") as file:
         config_data = json.load(file)
@@ -45,3 +36,6 @@ def valid_applescript_filepaths_config_path():
     with path.open("w", encoding="utf-8") as file:
         json.dump(config_data, file, indent=2)
         file.close()
+
+def _path(path):
+    return (Path(__file__).parent / path).resolve()
